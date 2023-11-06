b453752ab37f516423f07f226d33c77476ce28f5
Fauzan Abdillah <fauzan.abdillah2705@gmail.com>
2023-11-06 13:22:14 +0700

    adjust folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:implement_atomic_design/ui/pages/home_page.dart';
+import 'package:implement_atomic_design/ui/home/pages/home_page.dart';
 
 import 'core/constants/colors.dart';
 
